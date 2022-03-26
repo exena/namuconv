d48e5d3836171deea32ea7aa9d7dcf1152b3b9ff
exena <exena@naver.com>
2022-03-26 14:11:04 +0900

    namuconv

---
+++ quill-config.js
@@ -0,0 +1,52 @@
+//**********************************************************************************************
+//Check if input is url or document name.
+var Link = Quill.import('formats/link');
+Link.sanitize = function(url) {
+  let protocol = url.slice(0, url.indexOf(':'));
+  if (this.PROTOCOL_WHITELIST.indexOf(protocol) === -1) {
+    url = 'https://namu.wiki/w/' + url;
+  }
+  return url;
+}
+Quill.register(Link, true);
+//**********************************************************************************************
+//Make p block default font size "0.9rem".
+const BlockPrototype = Quill.import("blots/block");
+
+class CustomBlock extends BlockPrototype {
+  constructor(domNode, value) {
+    super(domNode, value);
+    this.format("size", "0.9rem");
+  }
+
+  static tagName = "P";
+
+  format(name, value) {
+    if (name === "size") {
+      this.domNode.style.fontSize = value;
+    } else {
+      super.format(name, value);
+    }
+  }
+}
+
+Quill.register(CustomBlock, true);
+//**********************************************************************************************
+//custom font size
+var Size = Quill.import('attributors/style/size');
+Size.whitelist = ['0.56rem','0.583rem','0.668rem','0.75rem','0.833rem','0.9rem','1.16rem','1.25rem','1.332rem','1.417rem','1.5rem'];
+Quill.register(Size, true);
+//**********************************************************************************************
+var quill;
+$('.quill-editor').each(function(i, el) {//index, element
+    var el = $(this), id = 'quilleditor-' + i, val = el.val(), editor_height = '100%';
+    var div = $('<div/>').attr('id', id).css('height', editor_height).html(val);
+    el.addClass('d-none');
+    el.parent().append(div);
+
+    quill = new Quill('#' + id, {
+        modules:{ toolbar: '#toolbar'},
+        theme: 'snow'
+    });
+    
+});
